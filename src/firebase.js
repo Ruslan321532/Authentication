964e6c5d537478be59bd425caf6f29c5166b0171
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-02-09 15:02:47 +0600

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrbs9JTiiCvaRN5EHmb6tbV3YiQoPYPx0",
-  authDomain: "gogletest-1e5f0.firebaseapp.com",
-  projectId: "gogletest-1e5f0",
-  storageBucket: "gogletest-1e5f0.appspot.com",
-  messagingSenderId: "897439102343",
-  appId: "1:897439102343:web:3dcbda622a4005535f4f22"
+  apiKey: "AIzaSyD-xNMS3x2tL6r4B9nuBQHV5Sn6jvtbqjA",
+  authDomain: "newapi-11bd1.firebaseapp.com",
+  projectId: "newapi-11bd1",
+  storageBucket: "newapi-11bd1.appspot.com",
+  messagingSenderId: "508231654422",
+  appId: "1:508231654422:web:890ac4f799576928fb2937"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
